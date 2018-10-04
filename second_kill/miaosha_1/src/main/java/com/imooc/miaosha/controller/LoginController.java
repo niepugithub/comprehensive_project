b1b9c483c13917e4e0157eb09b396e2b319702ef
niepu1991 <niepu1991@163.com>
2018-10-04 20:24:24 +0800

    添加apache MD5Util工具类

---
+++ second_kill/miaosha_1/src/main/java/com/imooc/miaosha/controller/LoginController.java
@@ -0,0 +1,10 @@
+package com.imooc.miaosha.controller;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/4 20:17
+ **/
+public class LoginController {
+}
---
+++ second_kill/miaosha_1/src/main/java/com/imooc/miaosha/util/MD5Util.java
@@ -0,0 +1,39 @@
+package com.imooc.miaosha.util;
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/4 20:00
+ **/
+public class MD5Util {
+    public static String md5(String src){
+        return DigestUtils.md5Hex(src);
+    }
+
+    private static final String salt="1a2b3c4d";
+    // 用户输入密码转化为表单密码
+    public static String inputPassFormPass(String inputPass){
+        String str = salt.charAt(0)+salt.charAt(2)+inputPass+salt.charAt(5)+salt.charAt(4);
+        return md5(str);
+    }
+    // 表单密码转化为DB密码
+    public static String formPassDBPass(String formPass,String salt){
+        String str = salt.charAt(0)+salt.charAt(2)+formPass+salt.charAt(5)+salt.charAt(4);
+        return md5(str);
+    }
+
+    // 用户输入密码转化为DB密码
+    public static String inputPassDBPass(String input,String salt){
+        String formPass=inputPassFormPass(input);
+        String dbPass = formPassDBPass(formPass,salt);
+        return dbPass;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(inputPassFormPass("123456"));
+        System.out.println(formPassDBPass("123456","1a2b3c4d"));
+        System.out.println(inputPassDBPass("123456","1a2b3c4d"));
+    }
+}
