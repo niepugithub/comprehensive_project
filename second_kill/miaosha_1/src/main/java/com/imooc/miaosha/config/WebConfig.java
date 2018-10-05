6343bdc106432ceb8453562bc9d9ddfc26d35fd0
niepu1991 <niepu1991@163.com>
2018-10-05 14:20:38 +0800

    argumentResolver实现统一session(用户信息)拦截处理

---
+++ second_kill/miaosha_1/src/main/java/com/imooc/miaosha/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.imooc.miaosha.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/5 14:02
+ **/
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter{
+    @Autowired
+    private UserArgumentResolver userArgumentResolver;
+    // 这个方法是框架给方法参数入参赋值的方法
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+         // super.addArgumentResolvers(argumentResolvers);
+        argumentResolvers.add(userArgumentResolver);
+    }
+
+}
