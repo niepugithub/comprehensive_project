eb4b1627f8ee8cfb1454d25c37debbb30b3b4375
niepu1991 <niepu1991@163.com>
2018-12-01 21:44:45 +0800

    创建search服务之solrj导入数据逻辑完成

---
+++ e3mall/e3-search/e3-search-interface/src/main/java/cn/e3mall/search/service/SearchItemService.java
@@ -0,0 +1,13 @@
+package cn.e3mall.search.service;
+
+import cn.e3mall.common.utils.E3Result;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/12/1 21:32
+ **/
+public interface SearchItemService {
+    E3Result importAllItems();
+}
