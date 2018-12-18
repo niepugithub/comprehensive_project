8a0ded7fc2368e8cfcaa65ee2868c51dad3986c7
niepu1991 <niepu1991@163.com>
2018-12-18 22:34:42 +0800

    e3-portal-web登录和注册url修改

---
+++ e3mall/e3-sso/e3-sso-interface/src/main/java/cn/e3mall/sso/service/TokenService.java
@@ -0,0 +1,10 @@
+package cn.e3mall.sso.service;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/12/18 22:22
+ **/
+public class TokenService {
+}
---
+++ e3mall/e3-sso/e3-sso-service/src/main/java/cn/e3mall/sso/service/impl/TokenServiceImpl.java
@@ -0,0 +1,10 @@
+package cn.e3mall.sso.service.impl;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/12/18 22:24
+ **/
+public class TokenServiceImpl {
+}
