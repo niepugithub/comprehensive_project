2ae2c0867d3c5b1cdbe75ae629c18052ed86fb24
niepu <niepu1991@163.com>
2018-12-26 23:12:02 +0800

    订单提交-service

--- e3mall/e3-order/e3-order-interface/src/main/java/cn/e3mall/order/service/OrderService.java
+++ e3mall/e3-order/e3-order-interface/src/main/java/cn/e3mall/order/service/OrderService.java
@@ -1,10 +1,14 @@
 package cn.e3mall.order.service;
 
+import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.order.pojo.OrderInfo;
+
 /**
  * @description:
  * @author:niepu
  * @version:1.0
  * @date:2018/12/26 22:55
  **/
-public class OrderService {
+public interface OrderService {
+    E3Result createOrder(OrderInfo orderInfo);
 }
