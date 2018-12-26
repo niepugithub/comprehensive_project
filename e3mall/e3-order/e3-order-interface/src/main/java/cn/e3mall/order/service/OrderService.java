4255b17c404a84aa7fc0a05b044843d55d73ad8d
niepu <niepu1991@163.com>
2018-12-26 22:52:41 +0800

    OrderInfo创建

---
+++ e3mall/e3-order/e3-order-interface/src/main/java/cn/e3mall/order/service/OrderService.java
@@ -0,0 +1,10 @@
+package cn.e3mall.order.service;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/12/26 22:55
+ **/
+public class OrderService {
+}
---
+++ e3mall/e3-order/e3-order-service/src/main/java/cn/e3mall/order/service/impl/OrderServiceImpl.java
@@ -0,0 +1,10 @@
+package cn.e3mall.order.service.impl;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/12/26 22:56
+ **/
+public class OrderServiceImpl {
+}
